83a5eb8e91fdc0f6c284b9d6a8f75215993f04b2
gdc94 <germanderbescatoni@gmail.com>
2023-07-13 20:29:51 -0300

    proof

--- services/hooks/useGetUserKey.ts
+++ services/hooks/useGetUserKey.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useSWR from "swr";
 import BASE_URL from "config/baseUrl";
 import { getUrlLogin } from "services/urls";
-import { FetcherProps, bodyRequestApiKey } from "typings/requests";
+import { bodyRequestApiKey } from "typings/requests";
 import { PostApiKeyResponse } from "typings/responses";
 
 /**
@@ -12,13 +12,14 @@ import { PostApiKeyResponse } from "typings/responses";
 
 const useGetUserKey = () => {
   const API_URL = getUrlLogin(BASE_URL);
+  
   const body: bodyRequestApiKey = {
     nombre: "German",
     email: "german@challengepaisanos.com",
   };
 
-  const fetcherPost = async (url: FetcherProps["url"]) => {
-    return await axios.post(url, body).then((res) => res.data);
+  const fetcherPost = async () => {
+    return await axios.post(API_URL, body).then((res) => res.data);
   };
 
   const { data, error, ...rest } = useSWR<PostApiKeyResponse>({
